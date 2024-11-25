d58e40d0d6e9d1854c1b0de0b0186ccddeb3f8c5
diana-potupis-vinci <diana.potupis@student.vinci.be>
2024-11-25 23:42:51 +0100

    new:ex2.14b+

--- exercices/2.14b/src/Components/App/App.tsx
+++ exercices/2.14b/src/Components/App/App.tsx
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
 import RandomDog from "../RandomDog";
 import "./App.css";
 
 function App() {
-  const [keys, setKeys] = useState([0, 1, 2]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setKeys([Math.random(), Math.random(), Math.random()]);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div>
       <h1>Random Dog</h1>
-      <RandomDog key={keys[0]} />
-      <RandomDog key={keys[1]} />
-      <RandomDog key={keys[2]} />
+      <RandomDog />
+      <RandomDog />
+      <RandomDog />
     </div>
   );
 }
--- exercices/2.14b/src/Components/RandomDog/index.tsx
+++ exercices/2.14b/src/Components/RandomDog/index.tsx
@@ -6,9 +6,20 @@ interface DogPhoto {
 
 function RandomDog() {
   const [dogPhoto, setDogPhoto] = useState<DogPhoto | null>(null);
+  const [isPaused, setIsPaused] = useState(false);
+
   useEffect(() => {
-    fetchDogs();
-  }, []);
+    if (!isPaused) {
+      fetchDogs();
+      const interval = setInterval(() => {
+        if (!isPaused) {
+          fetchDogs();
+        }
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isPaused]);
 
   const fetchDogs = async () => {
     try {
@@ -23,12 +34,23 @@ function RandomDog() {
       console.error(error);
     }
   };
+
+  const handleMouseEnter = () => {
+    setIsPaused(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsPaused(false);
+  };
+
   return (
     dogPhoto && (
       <img
         src={dogPhoto?.message}
         alt="Random dog"
         style={{ width: "300px", height: "auto" }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       />
     )
   );
